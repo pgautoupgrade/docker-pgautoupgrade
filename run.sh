f7a63cf544675c1d29d3ab0ede90788cd6e3a65c
Justin Clift <justin@postgresql.org>
2023-07-08 19:42:11 +1000

    Create the subdirectory for PG data, needed for a silly Docker bug
    This has been affecting people for years it seems:
    
        https://github.com/docker/for-linux/issues/380

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Create a sub-directory to hold the PG data, due to a really silly Docker bug that they're ignoring
+if [ ! -d pgstuff ]; then
+    mkdir pgstuff
+fi
+
 # Start the docker container 
 docker run --name pgauto -it --rm -e POSTGRES_PASSWORD=password --mount type=bind,source=$(pwd)/pgstuff/postgres-data,target=/var/lib/postgresql/data pgautoupgrade:latest
