6b56c2614a4291cd197de4efa0f4f42d6f3b2374
Justin Clift <justin@postgresql.org>
2023-07-14 21:55:42 +1000

    run.sh now uses the PG data directory in the test subdir

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+if [ ! -d "test/postgres-data" ]; then
+  mkdir test/postgres-data
+fi
+
 # Start the docker container 
-docker run --name pgauto -it --rm -e POSTGRES_PASSWORD=password --mount type=bind,source=$(pwd)/postgres-data,target=/var/lib/postgresql/data pgautoupgrade/pgautoupgrade:dev
+docker run --name pgauto -it --rm -e POSTGRES_PASSWORD=password --mount type=bind,source=$(pwd)/test/postgres-data,target=/var/lib/postgresql/data pgautoupgrade/pgautoupgrade:dev
