46547bbb359580626263ec11da1aab5ec9cc4942
Justin Clift <justin@postgresql.org>
2023-08-15 16:40:58 +1000

    Use a Makefile instead of lots of shell scripts (#11)

--- rm.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker image rm pgautoupgrade/pgautoupgrade:dev
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -d "test/postgres-data" ]; then
-  mkdir test/postgres-data
-fi
-
-# Start the docker container 
-docker run --name pgauto -it --rm -e POSTGRES_PASSWORD=password --mount type=bind,source=$(pwd)/test/postgres-data,target=/var/lib/postgresql/data $@ pgautoupgrade/pgautoupgrade:dev
