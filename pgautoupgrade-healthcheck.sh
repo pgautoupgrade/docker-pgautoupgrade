485a5286d2d7eb73ead9b2f42d4721e7d91e3ca1
Andy Pfister <andy.pfister@hey.com>
2025-02-06 10:36:07 +0100

    Allow to read Postgres username and database from Swarm secret files

--- pgautoupgrade-healthcheck.sh
+++ pgautoupgrade-healthcheck.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+read_env_or_file() {
+    local var_name="$1"
+    local file_var_name="${var_name}_FILE"
+
+    # Check if the *_FILE environment variable is set and points to a valid file
+    if [[ -n "${!file_var_name}" && -f "${!file_var_name}" && -s "${!file_var_name}" ]]; then
+        # Read the content of the file and assign it to the variable
+        echo "$(cat "${!file_var_name}")"
+    else
+        # Fallback to the normal environment variable
+        echo "${!var_name}"
+    fi
+}
+
 # Define the path to the upgrade lock file using PGDATA if set, otherwise default
 UPGRADE_LOCK_FILE="${PGDATA:-/var/lib/postgresql/data}/upgrade_in_progress.lock"
 
@@ -8,7 +22,10 @@ if [ -f "$UPGRADE_LOCK_FILE" ]; then
     exit 0
 fi
 
-pg_isready -d "${POSTGRES_DB}" -U "${POSTGRES_USER}"
+POSTGRES_DB_VALUE=$(read_env_or_file "POSTGRES_DB")
+POSTGRES_USER_VALUE=$(read_env_or_file "POSTGRES_USER")
+
+pg_isready -d "${POSTGRES_DB_VALUE}" -U "${POSTGRES_USER_VALUE}"
 
 # Capture the exit status of pg_isready
 PG_ISREADY_STATUS=$?
